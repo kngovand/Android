585e446e7129bcee6ecfdc1c74d78186bd35dfc6
K Duc <kngovand@gmail.com>
2020-06-23 17:00:30 -0600

    UI Working, now logic...

--- CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onConvert(View view) {
+        double result = 0;
+        TextView text = (TextView) findViewById(R.id.text2);
+
+                Toast.makeText(getApplicationContext(), "Hi bitches " + result, Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
