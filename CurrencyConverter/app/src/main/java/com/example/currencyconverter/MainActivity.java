4e9e2a196a83ba45b5a0fb31ac3b8ab145be99a2
K Duc <kngovand@gmail.com>
2020-06-23 20:06:31 -0600

    currency converter now working,

--- CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void onConvert(View view) {
+        double rate = 0.884204; // as of 6/23/2020
         double result = 0;
-        TextView text = (TextView) findViewById(R.id.text2);
+        String resultString = "";
 
-                Toast.makeText(getApplicationContext(), "Hi bitches " + result, Toast.LENGTH_LONG).show();
+        EditText text = findViewById(R.id.text2);
+        result = Double.parseDouble(text.getText().toString()) * rate;
+        resultString = "In euros: " + result;
+
+        Toast.makeText(getApplicationContext(), resultString, Toast.LENGTH_LONG).show();
 
     }
 
