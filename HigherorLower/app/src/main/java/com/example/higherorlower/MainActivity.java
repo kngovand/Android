2edf27c973eabcf1c9c309975154ac58266eb521
K Duc <kngovand@gmail.com>
2020-06-23 20:43:16 -0600

    Higher or Lower app done.

--- HigherorLower/app/src/main/java/com/example/higherorlower/MainActivity.java
+++ HigherorLower/app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -3,8 +3,29 @@ package com.example.higherorlower;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClick(View view) {
+        EditText text = findViewById(R.id.editText);
+        int number = Integer.parseInt(text.getText().toString());
+        int guessedNumber = (int)(Math.random()*20);
+
+        if(number == guessedNumber) {
+            Toast.makeText(
+                    getApplicationContext(), "Congrats you guessed right! Number = " + number, Toast.LENGTH_LONG).show();
+        }
+
+        else {
+            Toast.makeText(
+                    getApplicationContext(), "Try again... Correct Number = " + guessedNumber, Toast.LENGTH_LONG).show();
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
