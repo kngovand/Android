660b7263d8c586a7e5435fdd9bac0af20672be9e
K Duc <kngovand@gmail.com>
2020-06-25 01:13:05 -0600

    logs for debug added, tags added for imageview logic

--- Connect3/app/src/main/java/com/example/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/example/connect3/MainActivity.java
@@ -3,25 +3,43 @@ package com.example.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
 
+    TextView view1 = findViewById(R.id.textView);
+
+    ImageView image1 = findViewById(R.id.imageView1);
+    ImageView image2 = findViewById(R.id.imageView2);
+    ImageView image3 = findViewById(R.id.imageView3);
+    ImageView image4 = findViewById(R.id.imageView4);
+    ImageView image5 = findViewById(R.id.imageView5);
+    ImageView image6 = findViewById(R.id.imageView6);
+    ImageView image7 = findViewById(R.id.imageView7);
+    ImageView image8 = findViewById(R.id.imageView8);
+    ImageView image9 = findViewById(R.id.imageView9);
+
     public void dropIn(View view) {
+
+        Log.i("Tag","dropIn method called.");
+
         ImageView counter = (ImageView)(view);
-        TextView view1 = findViewById(R.id.textView);
 
         if(activePlayer == 0) {
+            Log.i("Tag","White");
             counter.setImageResource(R.drawable.chess_white);
             view1.setText("Black turn.");
             activePlayer = 1;
         }
 
         else {
+            Log.i("Tag","Black");
             counter.setImageResource(R.drawable.chess_black);
             view1.setText("White turn.");
             activePlayer = 0;
@@ -30,18 +48,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void onReset(View view) {
 
-        activePlayer = 0;
+        Log.i("Tag","onReset method called.");
 
-        TextView view1 = findViewById(R.id.textView);
-        ImageView image1 = findViewById(R.id.imageView1);
-        ImageView image2 = findViewById(R.id.imageView2);
-        ImageView image3 = findViewById(R.id.imageView3);
-        ImageView image4 = findViewById(R.id.imageView4);
-        ImageView image5 = findViewById(R.id.imageView5);
-        ImageView image6 = findViewById(R.id.imageView6);
-        ImageView image7 = findViewById(R.id.imageView7);
-        ImageView image8 = findViewById(R.id.imageView8);
-        ImageView image9 = findViewById(R.id.imageView9);
+        activePlayer = 0;
 
         image1.setImageResource(0);
         image2.setImageResource(0);
