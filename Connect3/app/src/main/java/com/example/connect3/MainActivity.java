faabc76ac1a03af81d3193681e4df7fdd94bdc14
K Duc <kngovand@gmail.com>
2020-06-25 01:34:04 -0600

    Final update before game logic complete

--- Connect3/app/src/main/java/com/example/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/example/connect3/MainActivity.java
@@ -12,37 +12,42 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
+    int[] board = {2,2,2,2,2,2,2,2,2};
+    int[][] wins;
 
-    TextView view1 = findViewById(R.id.textView);
-
-    ImageView image1 = findViewById(R.id.imageView1);
-    ImageView image2 = findViewById(R.id.imageView2);
-    ImageView image3 = findViewById(R.id.imageView3);
-    ImageView image4 = findViewById(R.id.imageView4);
-    ImageView image5 = findViewById(R.id.imageView5);
-    ImageView image6 = findViewById(R.id.imageView6);
-    ImageView image7 = findViewById(R.id.imageView7);
-    ImageView image8 = findViewById(R.id.imageView8);
-    ImageView image9 = findViewById(R.id.imageView9);
 
     public void dropIn(View view) {
 
+        for(int i : board) {
+            System.out.println(i);
+        }
+
         Log.i("Tag","dropIn method called.");
 
-        ImageView counter = (ImageView)(view);
+        TextView view1 = findViewById(R.id.textView);
+
+        ImageView image = (ImageView)(view);
 
         if(activePlayer == 0) {
-            Log.i("Tag","White");
-            counter.setImageResource(R.drawable.chess_white);
+            int counter = Integer.parseInt(image.getTag().toString());
+            image.setImageResource(R.drawable.chess_white);
+            board[counter] = 0;
             view1.setText("Black turn.");
             activePlayer = 1;
+
+            Log.i("Tag","White ");
+
         }
 
         else {
-            Log.i("Tag","Black");
-            counter.setImageResource(R.drawable.chess_black);
+            int counter = Integer.parseInt(image.getTag().toString());
+            image.setImageResource(R.drawable.chess_black);
+            board[counter] = 1;
             view1.setText("White turn.");
             activePlayer = 0;
+
+            Log.i("Tag","Black " );
+
         }
     }
 
@@ -51,6 +56,18 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Tag","onReset method called.");
 
         activePlayer = 0;
+
+        TextView view1 = findViewById(R.id.textView);
+
+        ImageView image1 = findViewById(R.id.imageView1);
+        ImageView image2 = findViewById(R.id.imageView2);
+        ImageView image3 = findViewById(R.id.imageView3);
+        ImageView image4 = findViewById(R.id.imageView4);
+        ImageView image5 = findViewById(R.id.imageView5);
+        ImageView image6 = findViewById(R.id.imageView6);
+        ImageView image7 = findViewById(R.id.imageView7);
+        ImageView image8 = findViewById(R.id.imageView8);
+        ImageView image9 = findViewById(R.id.imageView9);
 
         image1.setImageResource(0);
         image2.setImageResource(0);
