44dd6fb14b2662b9062028bd34da16f2e5b99e8e
K Duc <kngovand@gmail.com>
2020-06-25 17:32:11 -0600

    debug info for Connect 3, cleaned up house

--- Connect3/app/src/main/java/com/example/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/example/connect3/MainActivity.java
@@ -9,18 +9,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-    int activePlayer = 0;
-    int[] board = {2,2,2,2,2,2,2,2,2};
-    int[][] wins = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+    int activePlayer = 0; // 0 - white, 1 - black, 2 - null
+    int[] board = {2,2,2,2,2,2,2,2,2}; // 3x3 grid, initialized to null values
+    int[][] wins = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}}; // winning patterns
 
-    public void resetBoard() {
-        for (int i : board) {
-            board[i] = 2;
-        }
-    }
-
+    // method when imageview on grid is clicked
     public void dropIn(View view) {
 
         Log.i("Tag","dropIn method called.");
@@ -29,48 +26,60 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView image = (ImageView)(view);
 
+        // white turn
         if(activePlayer == 0) {
+
             int counter = Integer.parseInt(image.getTag().toString());
+
             image.setImageResource(R.drawable.chess_white);
+
             board[counter] = 0;
-            view1.setText("Black turn.");
             activePlayer = 1;
 
-            Log.i("Tag","White " + counter);
+            Log.i("Tag","White, " + "board index: " + counter);
+
+            view1.setText("Black turn.");
         }
 
+        // black turn
         else {
             int counter = Integer.parseInt(image.getTag().toString());
+
             image.setImageResource(R.drawable.chess_black);
-            board[counter] = 1;
-            view1.setText("White turn.");
+
+            board[counter] = 0;
             activePlayer = 0;
 
-            Log.i("Tag","Black " + counter );
+            Log.i("Tag","Black, " + "board index: " + counter);
+
+            view1.setText("Black turn.");
         }
 
+        // to check if grid has winning positions?
         for (int[] winningPosition : wins) {
             if(board[winningPosition[0]] == board[winningPosition[1]] && board[winningPosition[1]] == board[winningPosition[2]] && board[winningPosition[0]] != 2) {
 
-                String winner = "";
-
+                // white winner!
                 if(activePlayer == 0) {
-                    winner = "Black";
-                    Toast.makeText(getApplicationContext(), winner + " has won!", Toast.LENGTH_LONG).show();
-                    view1.setText("Click reset to play again!");
-                    resetBoard();
-                }
-                else {
-                    winner = "White";
-                    Toast.makeText(getApplicationContext(), winner + " has won!", Toast.LENGTH_LONG).show();
-                    view1.setText("Click reset to play again!");
+                    Log.i("Tag","array: " + Arrays.toString(winningPosition));
+
+                    Toast.makeText(getApplicationContext(), "White has won!", Toast.LENGTH_LONG).show();
                     resetBoard();
                 }
 
+                else {
+                    Log.i("Tag","array: " + Arrays.toString(winningPosition));
+
+                    Toast.makeText(getApplicationContext(), "Black has won!", Toast.LENGTH_LONG).show();
+                    resetBoard();
+                }
+
+                view1.setText("Click RESET to play again!");
             }
         }
     }
 
+    // resets 3x3 grid by clearing imageviews and resets board array
     public void onReset(View view) {
 
         Log.i("Tag","onReset method called.");
@@ -78,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         activePlayer = 0;
 
         TextView view1 = findViewById(R.id.textView);
-
+        // is there a better way to write this?
         ImageView image1 = findViewById(R.id.imageView1);
         ImageView image2 = findViewById(R.id.imageView2);
         ImageView image3 = findViewById(R.id.imageView3);
@@ -103,6 +112,16 @@ public class MainActivity extends AppCompatActivity {
 
         resetBoard();
     }
+
+    // resets board array to null values again
+    public void resetBoard() {
+        Log.i("Tag", "resetBoard method called");
+
+        for (int i : board) {
+            board[i] = 2; // to null value again
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
