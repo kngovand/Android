2c14ba0660bb298a8023ff8e0da97cbf120d8cf1
K Duc <kngovand@gmail.com>
2020-06-26 16:41:30 -0600

    seekbar logic complete, sound demo done!

--- SoundDemo/app/src/main/java/com/example/sounddemo/MainActivity.java
+++ SoundDemo/app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.sounddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,6 +16,8 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+
+    AudioManager audioManager;
 
     public void play(View view) {
         TextView view1 = (TextView) findViewById(R.id.textView);
@@ -30,6 +36,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.tutu);
+
+        SeekBar volume = findViewById(R.id.seekBar);
+
+        volume.setMax(maxVol);
+        volume.setProgress(currentVol);
+
+        volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("tag", Integer.toString(progress));
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // scrub bar for more accuracy?
     }
 }
