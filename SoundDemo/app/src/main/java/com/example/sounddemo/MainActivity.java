44fab500b462c6339ad722084dd4bc22d256ef7a
K Duc <kngovand@gmail.com>
2020-06-26 13:39:37 -0600

    basic logic complete, todo: seeker logic

--- SoundDemo/app/src/main/java/com/example/sounddemo/MainActivity.java
+++ SoundDemo/app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.sounddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    public void play(View view) {
+        TextView view1 = (TextView) findViewById(R.id.textView);
+        mediaPlayer.start();
+        view1.setText("Tu tu tu tu");
+    }
+
+    public void pause(View view) {
+        TextView view1 = (TextView) findViewById(R.id.textView);
+        mediaPlayer.pause();
+        view1.setText("It's quiet here...");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.tutu);
     }
 }
