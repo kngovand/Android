c26ff5380d8659205a547a86c7638abe0dd6e971
K Duc <kngovand@gmail.com>
2020-07-01 15:11:47 -0600

    listview and arrayadapter added.

--- ListviewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ ListviewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.ListView);
+
+        ArrayList<String> array = new ArrayList<>();
+
+        array.add("john");
+        array.add("mary");
+        array.add("cody");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
+
+        listView.setAdapter(arrayAdapter);
+
     }
 }
