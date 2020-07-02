1ab461156147b5f7f7dac61820a0dcdd8ad98ecf
K Duc <kngovand@gmail.com>
2020-07-02 06:12:24 -0600

    TimesTable complete: listview, array. Todo: seekbar logic

--- TimesTable/app/src/main/java/com/example/timestable/MainActivity.java
+++ TimesTable/app/src/main/java/com/example/timestable/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.timestable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, array);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
