ce64d7db233ba26e9d6c0b85e84b5e5a760d84e4
K Duc <kngovand@gmail.com>
2020-07-02 06:16:11 -0600

    TimesTable complete.

--- TimesTable/app/src/main/java/com/example/timestable/MainActivity.java
+++ TimesTable/app/src/main/java/com/example/timestable/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.timestable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,12 +19,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        final ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-        ListView listView = findViewById(R.id.listView);
+        final ListView listView = findViewById(R.id.listView);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        final TextView textView = findViewById(R.id.textView);
 
-        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, array);
-
+        final ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, array);
         listView.setAdapter(arrayAdapter);
+
+        // scroll from 0-10
+        seekBar.setMax(9);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("tag", "" + progress);
+
+                textView.setText("Times " + progress);
+
+                for(int i = 0; i < array.size(); i++) {
+                    array.set(i, (i+1) * progress);
+                }
+
+                //reset adapter to update array values
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
