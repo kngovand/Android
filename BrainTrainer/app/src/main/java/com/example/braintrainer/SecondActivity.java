ad863ff238d5a8ed816108a796c0a64e2b31ceee
K Duc <kngovand@gmail.com>
2020-08-25 17:05:44 -0600

    logic mostly complete, needs reset on answer pick now

--- BrainTrainer/app/src/main/java/com/example/braintrainer/SecondActivity.java
+++ BrainTrainer/app/src/main/java/com/example/braintrainer/SecondActivity.java
@@ -4,17 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class SecondActivity extends AppCompatActivity {
 
+    ArrayList<Integer> answers = new ArrayList<>();
+
+    Random rand = new Random();
+    int correctIndex = rand.nextInt(4);
+
     public void chooseAnswer(View view) {
-        // testing input
-        Toast.makeText(this,"hi", Toast.LENGTH_LONG).show();
+        TextView answerTextView = findViewById(R.id.answerTextView);
+
+        Log.i("tag", view.getTag().toString());
+
+        if(view.getTag().toString().equals(String.valueOf(correctIndex)) ) {
+            Toast.makeText(this, "yes", Toast.LENGTH_SHORT).show();
+            answerTextView.setText("Correct!");
+        }
+        else {
+            answerTextView.setText("Wrong!");
+        }
     }
 
     @Override
@@ -23,13 +40,41 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         TextView sumTextView = findViewById(R.id.sumTextView);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
 
-        Random rand = new Random();
+
 
         // max of 21
         int a = rand.nextInt(21);
         int b = rand.nextInt(21);
 
-        //sumTextView.setText(a + " + " + b);
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        // random to 3 for placing correct answer in button position
+        correctIndex = rand.nextInt(4);
+
+        for (int i = 0; i < 4; i++) {
+            if(i == correctIndex) {
+                // for correct answer
+                answers.add(a + b);
+            }
+            else {
+                int wrongAnswer = rand.nextInt(41);
+                // in case wrong answer accidentally rolled to right answer
+                while(wrongAnswer == a + b) {
+                    wrongAnswer = rand.nextInt(41);
+                }
+              // for wrong answers
+              answers.add(rand.nextInt(41));
+            }
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 }
